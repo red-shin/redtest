37994a609ef8a0972350d924f76f68d03a09af36
rshin <ryonsu.shin@redmagic.co.jp>
2023-12-06 20:18:25 +0900

    update

--- main.py
+++ main.py
@@ -22,4 +22,50 @@ OPENAI_CHARACTER_PROFILE = '''
 '''
 
 
+openai.api_key = OPENAI_API_KEY
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+line_parser = WebhookParser(LINE_CHANNEL_SECRET)
+app = FastAPI()
 
+
+@app.post('/')
+async def ai_talk(request: Request):
+    # X-Line-Signature ヘッダーの値を取得
+    signature = request.headers.get('X-Line-Signature', '')
+
+    # request body から event オブジェクトを取得
+    events = line_parser.parse((await request.body()).decode('utf-8'), signature)
+
+    # 各イベントの処理（※1つの Webhook に複数の Webhook イベントオブジェっｚクトが含まれる場合あるため）
+    for event in events:
+        if event.type != 'message':
+            continue
+        if event.message.type != 'text':
+            continue
+
+        # LINE パラメータの取得
+        line_user_id = event.source.user_id
+        line_message = event.message.text
+
+        # ChatGPT からトークデータを取得
+        response = openai.ChatCompletion.create(
+            model = 'gpt-3.5-turbo'
+            , temperature = 0.5
+            , messages = [
+                {
+                    'role': 'system'
+                    , 'content': OPENAI_CHARACTER_PROFILE.strip()
+                }
+                , {
+                    'role': 'user'
+                    , 'content': line_message
+                }
+            ]
+        )
+        ai_message = response['choices'][0]['message']['content']
+
+        # LINE メッセージの送信
+        line_bot_api.push_message(line_user_id, TextSendMessage(ai_message))
+
+    # LINE Webhook サーバーへ HTTP レスポンスを返す
+    return 'ok'
